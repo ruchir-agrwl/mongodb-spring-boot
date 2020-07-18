3288634766f21676c3f8f87bb55262a1087f238e
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-07-18 20:22:04 +0530

    Boiler Plate Code Added Closes #1

---
+++ src/main/java/com/learn/mongo/MongoMvpApplication.java
@@ -0,0 +1,13 @@
+package com.learn.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/mongo/MongoMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
